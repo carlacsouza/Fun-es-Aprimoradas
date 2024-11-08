6204e3b62758d5013db3348347efcae0343cd033
carlacsouza <107197543+carlacsouza@users.noreply.github.com>
2024-11-08 09:43:58 -0300

    04

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,10 @@
+const nomes = ["Amanda", "Carla", "Camila", "Caio", "Tamiris", "Ana", "Marcelo", "Marcos", "Silvana", "Aline", "Leonardo", "João", "Samanta", "Yuri", "Sara"];
+
+
+
 export function aleatorio(lista){
     const posicao = Math.floor(Math.random()*lista.length);
     return lista[posicao];
 }
+
+export const nome = aleatorio(nomes);
